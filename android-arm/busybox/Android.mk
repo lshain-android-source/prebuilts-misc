828af961db9a1751e360060fea1daf8453ff8749
lshain <lshain.gyh@gmail.com>
2014-02-08 15:52:29 +0800

    fix busybox Android.mk

--- android-arm/busybox/Android.mk
+++ android-arm/busybox/Android.mk
@@ -1,21 +1,13 @@
 LOCAL_PATH := $(call my-dir)
-
 include $(CLEAR_VARS)
 
-# We do not include source of busybox because of license.(GPL issues)
-
 LOCAL_SRC_FILES := busybox
-
 LOCAL_MODULE := busybox
-
 LOCAL_MODULE_CLASS := EXECUTABLES
-
-LOCAL_MODULE_TAGS := optional
-
+LOCAL_MODULE_TAGS := debug
 include $(BUILD_PREBUILT)
 
 # Any prebuilt files with default TAGS can use the below:
-
 prebuilt_files :=
 
 $(call add-prebuilt-files, EXECUTABLES, $(prebuilt_files))
